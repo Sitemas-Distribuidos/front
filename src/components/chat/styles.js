4445393c3a6773fb07adbe2acfd80d8926b3a28a
Beatriz-SMB <bsnbernardors@gmail.com>
2025-06-02 14:52:34 -0300

    style: add style to menu icon.

--- src/components/chat/styles.js
+++ src/components/chat/styles.js
@@ -2,79 +2,85 @@ import styled from "styled-components";
 import SVG from "react-inlinesvg";
 import colors from "../../utils/colors";
 
-export const Container = styled.main`   
-    align-items: flex-start;
-    background: ${colors.secondary};
-    box-shadow: 5px 5px 17px 1px rgba(0, 0, 0, 0.3);
-    color: #000;
+export const Container = styled.div`  
     display: flex;
-    flex-direction: column;
-    height: 600px;
-    justify-content: center;
-    padding: 20px;
-    width: 400px;
 
-    -webkit-box-shadow: 5px 5px 17px 1px rgba(0, 0, 0, 0.3);
-
-    .chat {
+    .chat-conatiner {
+        align-items: flex-start;
+        background: ${colors.secondary};
+        box-shadow: 5px 5px 17px 1px rgba(0, 0, 0, 0.3);
+        color: #000;
         display: flex;
         flex-direction: column;
-        height: 550px;
-        width: 100%;
-        
-        .message-container {
-            align-self: start;
-            background-color: lightgrey;
-            border-radius: 7px;
-            margin-top: 10px;
-            max-width: 250px;
-            padding: 5px 10px;
-            width: fit-content;
+        height: 600px;
+        justify-content: center;
+        padding: 20px;
+        width: 400px;
+
+        -webkit-box-shadow: 5px 5px 17px 1px rgba(0, 0, 0, 0.3);
+
+        .chat {
+            display: flex;
+            flex-direction: column;
+            height: 550px;
+            width: 100%;
+            
+            .message-container {
+                align-self: start;
+                background-color: lightgrey;
+                border-radius: 7px;
+                color: ${colors.secondary};
+                margin-top: 10px;
+                max-width: 250px;
+                padding: 5px 10px;
+                width: fit-content;
+            }
+
+            .message-mine{
+                align-self: end;
+                background-color: skyblue;
+                color: ${colors.secondary};
+            }
         }
 
-        .message-mine{
-            align-self: end;
-            background-color: skyblue;
-        }
-    }
-
-    .input-container {
-        align-items: center;
-        display: flex;
-        width: 100%;
-
-        div {
-            align-items: flex-end;
+        .input-container {
+            align-items: center;
             display: flex;
             width: 100%;
 
-            input {
-                background: ${colors.secondary};
-                border: none;
-                border-bottom: 2px solid ${colors.primary};
-                color: ${colors.primary};
-                outline: none;
+            div {
+                align-items: flex-end;
+                display: flex;
                 width: 100%;
-            }
-            
-            label {
-                display: inline-block;
+
+                input {
+                    background: ${colors.secondary};
+                    border: none;
+                    border-bottom: 2px solid ${colors.primary};
+                    color: ${colors.primary};
+                    outline: none;
+                    width: 100%;
+                }
+                
+                label {
+                    display: inline-block;
+                }
+
+                .hidden-input{
+                    display: none;
+                }
             }
 
-            .hidden-input{
-                display: none;
+            button {
+                align-items: center;
+                background: transparent;
+                border: none;
+                border-radius: 5px;
+                color: #000;
+                cursor: pointer;
+                display: flex;
+                outline: none;
             }
-        }
-
-        button {
-            align-items: center;
-            background: transparent;
-            border: none;
-            border-radius: 5px;
-            color: #000;
-            cursor: pointer;
-            display: flex;
-            outline: none;
         }
     }
 `;
