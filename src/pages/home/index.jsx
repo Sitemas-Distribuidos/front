8c341336800a3b9c9b745c9729384fc727084ade
Beatriz-SMB <bsnbernardors@gmail.com>
2025-06-06 17:47:32 -0300

    refactor: simplify Modal usage in Home component by removing static content

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -17,11 +17,7 @@ const Home = () => {
             <Container>
                 {/* <Join/> */}
                 <Chat />
-                <Modal>
-                    <h2>Esse é o conteúdo do modal!</h2>
-                    {/* <h2>Esse é o conteúdo do modal!</h2>
-                    <p>Você pode colocar qualquer coisa aqui.</p> */}
-                </Modal>
+                <Modal />
             </Container>
         </ModalProvider>
      );
