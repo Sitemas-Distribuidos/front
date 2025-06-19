4c66d6a2161d4287999c26fd61fa83bd804fbb12
Beatriz-SMB <bsnbernardors@gmail.com>
2025-06-19 16:35:28 -0300

    style: add styled components for Dropdown container and button icon

---
+++ src/components/dropdown/styles.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import SVG from "react-inlinesvg";
+import { colors } from "../../utils/colors";
+
+export const Container = styled.div`
+    background-color: #252422;
+    border-radius: 6px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 5px 0;
+    position: absolute;
+    text-align: center;
+    width: 120px;
+    z-index: 1;
+
+    button {
+        background: transparent;
+        border: none;
+        color: ${colors.primary};
+        cursor: pointer;
+        display: flex;
+        gap: 10px;
+        padding: 10px;
+        text-align: center;
+        text-decoration: none;
+        width: 100%;
+
+        &:hover {
+            background-color: #403D39;
+        }
+    }
+`
+
+export const ButtonIcon = styled(SVG)`
+    height: 20px;
+    width: 20px;
+    & path {
+        fill: #FFFCF2;
+    }
+`
