41fa37137b28bf30ede7a08b635c621e94b92663
Beatriz-SMB <bsnbernardors@gmail.com>
2025-06-19 14:03:40 -0300

    feat: implement loading state and handle contact addition in Add component

--- src/components/add/index.jsx
+++ src/components/add/index.jsx
@@ -1,3 +1,9 @@
+/* ⚛ REACT */
+import { useState, useContext  } from "react";
+
+/* 🧠 CONTEXT */
+import { ModalContext } from '../../context/ModalContext';
+
 /* 📁 ASSETS */
 import { add } from "../../assets/icons";
 
@@ -6,13 +12,27 @@ import { Container, AddIcon } from "./styles";
 
 
 const Add = () => {
+
+    const { closeModal } = useContext(ModalContext);
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSubmit = () => {
+        setIsLoading(true);
+        setTimeout(() => {
+            setIsLoading(false);
+            alert("Contact added successfully!");
+            closeModal();
+        }, 5000); 
+    }
+
     return (
         <Container>
             <h1>Add Contact</h1>
             <input type="text" placeholder="Type contact name" />
-            <button>
+            <button onClick={() => handleSubmit()} disabled={isLoading}>
                 <AddIcon src={add} />
-                Add
+                {isLoading ? "Adding..." : "Add"}
             </button>
         </Container>
     );
