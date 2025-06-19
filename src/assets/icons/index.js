b3dc3e81b7cda2062991a039be7dd1d26c045d62
Beatriz-SMB <bsnbernardors@gmail.com>
2025-06-19 14:03:29 -0300

    feat: add exclamation icon to the icons export

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,5 +1,6 @@
 import close from "./close.svg";
 import add from "./add.svg";
+import remove from "./remove.svg";
 import logout from "./logout.svg";
 import group from "./group.svg";
 import person from "./person.svg";
@@ -13,5 +14,6 @@ import addGroup from "./add-group.svg";
 import trash from "./trash.svg";
 import more from "./more.svg";
 import pin from "./pin.svg";
+import exclamation from "./exclamation.svg";
 
-export { close, add, logout, group, person, login, clip, menu, send, search, addPerson, addGroup, trash, more, pin };
+export { close, add, logout, group, person, login, clip, menu, send, search, addPerson, addGroup, trash, more, pin, remove, exclamation };
