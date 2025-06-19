73a71ac85da0cd927395b83b5b2a39d0ff2d7798
Beatriz-SMB <bsnbernardors@gmail.com>
2025-06-19 16:35:19 -0300

    feat: add Dropdown component with remove and edit functionality

---
+++ src/components/dropdown/index.jsx
@@ -0,0 +1,43 @@
+/* ⚛ REACT */
+import { useContext } from "react";
+
+/* 🧠 CONTEXT */
+import { MessageContext } from '../../context/MessageContext';
+
+/* 📁 ASSETS*/
+import { trash, edit } from "../../assets/icons";
+
+/* 🎨 STYLES */
+import { Container, ButtonIcon } from "./styles";
+
+const Dropdown = ({ chatId, onClose }) => {
+
+    const { showMessage } = useContext(MessageContext);
+
+    const handleRemove = () => {
+        console.log(`Removing chat with ID: ${chatId}`);
+        onClose(); 
+        showMessage('success', 'Chat removed successfully!');
+    }
+
+    const handleEdit = () => {
+        console.log(`Editing chat with ID: ${chatId}`);
+        onClose();
+        showMessage('warning', 'No chat selected for editing!');
+    }
+
+    return (
+        <Container>
+            <button onClick={() => handleRemove()}> 
+                <ButtonIcon src={trash} alt="Remove Icon"/>
+                Remover
+            </button>
+            <button onClick={() => handleEdit()}>
+                <ButtonIcon src={edit} alt="Remove Icon"/>
+                Editar
+            </button>
+        </Container>
+    )
+}
+
+export default Dropdown;
