62f6deae366727e2ab92c0304b3b1df812fe6fab
Beatriz-SMB <bsnbernardors@gmail.com>
2025-06-19 16:36:35 -0300

    feat: wrap Home component with MessageProvider for contextual messaging

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,9 +2,11 @@
 import Chat from "../../components/chat";
 import Join from "../../components/join";
 import Modal from "../../components/modal";
+import Message from "../../components/message";
 
 /* 🧠 CONTEXT */
 import { ModalProvider } from "../../context/ModalContext";
+import { MessageProvider } from "../../context/MessageContext";
 
 /* 🎨 STYLES */
 import { Container } from "./styles";
@@ -14,13 +16,16 @@ const Home = () => {
     document.title = "Chat";
 
     return ( 
+        <MessageProvider>
             <ModalProvider>
                 <Container>
+                    <Message />
                     {/* <Join/> */}
                     <Chat />
                     <Modal />
                 </Container>
             </ModalProvider>
+        </MessageProvider>
      );
 }
  
