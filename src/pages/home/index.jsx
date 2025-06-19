e46e955705a76af7735334dfeb5b1fbd44ab0624
Beatriz-SMB <bsnbernardors@gmail.com>
2025-06-19 14:06:29 -0300

    style: reorganize whitespace for improved readability in Home component

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,23 +3,24 @@ import Chat from "../../components/chat";
 import Join from "../../components/join";
 import Modal from "../../components/modal";
 
-
+/* 🧠 CONTEXT */
 import { ModalProvider } from "../../context/ModalContext";
 
 /* 🎨 STYLES */
 import { Container } from "./styles";
 
+
 const Home = () => {
     document.title = "Chat";
 
     return ( 
-        <ModalProvider>
-            <Container>
-                {/* <Join/> */}
-                <Chat />
-                <Modal />
-            </Container>
-        </ModalProvider>
+            <ModalProvider>
+                <Container>
+                    {/* <Join/> */}
+                    <Chat />
+                    <Modal />
+                </Container>
+            </ModalProvider>
      );
 }
  
