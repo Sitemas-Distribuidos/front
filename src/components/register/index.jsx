05797b52d39ba04adcbacd2c69e5e05075d05c3f
Beatriz-SMB <bsnbernardors@gmail.com>
2025-06-20 15:04:14 -0300

    feat: add Register component with form and loading state.

---
+++ src/components/register/index.jsx
@@ -0,0 +1,54 @@
+/* ⚛ REACT */
+import React, { useRef, useState } from "react";
+
+/* 📁 ASSETS*/
+import { register } from "../../assets/icons";
+
+/* 🎨 STYLES */
+import { Container, RegisterIcon } from "./styles";
+
+const Register = () => {
+
+    const usernameRef = useRef(null);
+    const userpasswordRef = useRef(null);
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleSubmit = async () => {
+        setIsLoading(true);
+        setTimeout(() => {
+            console.log(usernameRef.current.value);
+            setIsLoading(false);
+        }, 6000)
+        // const username = usernameRef.current.value;
+        // username && setChatVisibility(true);
+        
+    }
+
+    return(
+        <Container>
+            <h1>Register</h1>
+            <form>
+                <div>
+                    <label htmlFor="username">User Name</label>
+                    <input type="text" ref={usernameRef} />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" ref={userpasswordRef} />
+                </div>
+                <div>
+                    <label htmlFor="confirm-password">Confirm Password</label>
+                    <input type="password" ref={userpasswordRef} />
+                </div>
+                <a href="" >Already have an account?</a>
+            </form>
+            <button onClick={() => handleSubmit()}>
+                {isLoading ? 'Registering...' : 'Register'}
+                <RegisterIcon src={register} />
+            </button>
+        </Container>
+    );
+}
+
+export default Register;
