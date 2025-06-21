878d30e2a6af7073c0f7205d2e6446e068d4ffb6
Beatriz-SMB <bsnbernardors@gmail.com>
2025-06-20 21:06:39 -0300

    style: remove commented-out gradient background from Container.

--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -3,7 +3,6 @@ import { colors } from "../../utils/colors";
 
 export const Container = styled.main`   
     align-items: center;
-    /* background: linear-gradient(to right, #4facfe 0%, #00f2fe 100%); */
     background: ${colors.tertiary};
     display: flex;
     height: 100vh;
