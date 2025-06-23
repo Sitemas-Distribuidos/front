49115b8e3d189e2a04fd3509ec1d39718f2d4dc9
Beatriz-SMB <bsnbernardors@gmail.com>
2025-06-23 09:42:24 -0300

    feat: create global styles with styled-components for main container;

---
+++ src/styles/global.js
@@ -0,0 +1,14 @@
+/* 📦 LIBS */
+import styled from "styled-components";
+
+/* 🔧 UTILS */
+import { colors } from "../utils/colors";
+
+export const Container = styled.main`   
+    align-items: center;
+    background: ${colors.tertiary};
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    width: 100%;
+`;
