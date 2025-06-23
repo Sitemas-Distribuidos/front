881704b68e1e1a2372fd74aeaeacda59b1c6dabb
Beatriz-SMB <bsnbernardors@gmail.com>
2025-06-23 17:51:55 -0300

    refactor: simplify Sidebar component styles by removing unused animation;

--- src/components/menu/styles.js
+++ src/components/menu/styles.js
@@ -1,29 +1,18 @@
 /* 📦 LIBS */
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import SVG from "react-inlinesvg";
+import 'animate.css/animate.min.css';
 
 /* 🔧 UTILS */
 import { colors } from "../../utils/colors";
 
-const fadeInRight = keyframes`
-  from {
-    opacity: 0;
-    transform: translateX(100px);
-  }
-  to {
-    opacity: 1;
-    transform: translateX(0);
-  }
-`;
-
 export const Sidebar = styled.div` 
-    animation: ${fadeInRight} 0.5s ease forwards;
     background: ${colors.primary};
     display: flex;
     flex-direction: row;
     height: 100vh;
     width: 100%;
-
+    
     @media (min-width: 720px) {
         height: 600px;
         width: 300px;
